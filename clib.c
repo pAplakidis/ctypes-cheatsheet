4558a728b5212073e7d311f85ea9b45a84546910
pavlos.aplakidis <pavlos.aplakidis@scigentech.com>
2024-09-25 23:57:53 +0300

    playing with pointers

--- clib.c
+++ clib.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // gcc -fPIC -shared -o clib.so clib.c
 
@@ -8,3 +10,15 @@ char *display(char *str, int age) {
 }
 
 int add(int a, int b) { return a + b; }
+
+char *alloc_mem() {
+  char *str = strdup("Hello World");
+  printf("alloc_mem() DONE\n");
+  return str;
+}
+
+void free_mem(char *ptr) {
+  printf("free_mem() DONE\n");
+  free(ptr);
+  ptr = NULL;
+}
--- ctypes_tutorial.py
+++ ctypes_tutorial.py
@@ -16,3 +16,13 @@ add.argtypes = [ctypes.c_int, ctypes.c_int]
 add.restype = ctypes.c_int
 res = add(2, 2)
 print(res)
+
+alloc_mem = clib.alloc_mem
+alloc_mem.restype = ctypes.POINTER(ctypes.c_char_p)
+ptr = alloc_mem()
+cstring = ctypes.c_char_p.from_buffer(ptr)
+print(cstring.value)
+
+free_mem = clib.free_mem
+free_mem.argtypes = [ctypes.POINTER(ctypes.c_char_p)]
+free_mem(ptr)
