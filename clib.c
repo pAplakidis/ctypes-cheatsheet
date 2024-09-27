552220300b1d687d24f514588377042ae0a178a1
pavlos.aplakidis <aplakidisp@gmail.com>
2024-09-27 10:40:32 +0300

    arrays

--- clib.c
+++ clib.c
@@ -22,3 +22,34 @@ void free_mem(char *ptr) {
   free(ptr);
   ptr = NULL;
 }
+
+int sum(int *arr, int size) {
+  int sum = 0;
+  for (int i = 0; i < size; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+int *inc_array(int *arr, int size) {
+  for (int i = 0; i < size; i++) {
+    arr[i]++;
+  }
+  return arr;
+}
+
+// NOTE: we dynamically allocate the array so that we can return it without
+// scope problems (check WARNING if using stack variable)
+int *get_array() {
+  int *arr = malloc(10 * sizeof(int));
+  for (int i = 0; i < 10; i++) {
+    arr[i] = i;
+  }
+  return arr;
+}
+
+void free_array(int *arr) {
+  free(arr);
+  arr = NULL;
+  printf("Array freed\n");
+}
--- ctypes_tutorial.py
+++ ctypes_tutorial.py
@@ -5,18 +5,21 @@ clib = ctypes.CDLL("clib.so")
 
 # clib.display(b"John", 18)
 
+# Simple print
 display = clib.display
 display.argtypes = [ctypes.c_char_p, ctypes.c_int]
 display.restype = ctypes.c_char_p
 res = display(b"John", 18)
 print(res)
 
+# Simple return
 add = clib.add
 add.argtypes = [ctypes.c_int, ctypes.c_int]
 add.restype = ctypes.c_int
 res = add(2, 2)
 print(res)
 
+# Pointers and memory allocation
 alloc_mem = clib.alloc_mem
 alloc_mem.restype = ctypes.POINTER(ctypes.c_char_p)
 ptr = alloc_mem()
@@ -26,3 +29,26 @@ print(cstring.value)
 free_mem = clib.free_mem
 free_mem.argtypes = [ctypes.POINTER(ctypes.c_char_p)]
 free_mem(ptr)
+
+# Arrays
+values = (ctypes.c_int * 10)()  # int values[10];
+for i in range(len(values)):
+  values[i] = i
+res = clib.sum(values, len(values))
+print(res)
+
+# NOTE: always do this when returning a pointer
+clib.inc_array.restype = ctypes.POINTER(ctypes.c_int)
+res = clib.inc_array(values, len(values))
+print(res.contents) # print(*ptr)
+
+# NOTE: we cannot get the size from C, need to keep it somehow
+# print the whole array
+for i in range(10):
+  print(res[i])
+
+clib.get_array.restype = ctypes.POINTER(ctypes.c_int)
+res = clib.get_array()
+for i in range(10):
+  print(res[i])
+free_array = clib.free_array()
